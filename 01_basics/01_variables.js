9331700e7bb7e22c5e4ce16b08d435987e063d37
Prakhar <118461818+Prakhar07012005@users.noreply.github.com>
2023-08-13 11:03:26 +0000

    commit

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId =144598
+let accountEmail = "prakhar@google.com"
+var accountPassword = "8520"
+
+
+console.log(accountPassword);
+accountPassword=54484
+
+
+console.table([accountEmail,accountId,accountPassword])
+
